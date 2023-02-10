bfaaf435432b55d93a72862ff8a70aeb9dc3f0a5
sunrimii <sunrimii@gmail.com>
2023-02-10 08:55:16 +0800

    fix: change default shell to zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # zsh
 apt update && apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 
 # zsh autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
