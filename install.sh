a6a9ec8b44eef4aee672f68fc8dc705493384cef
sunrimii <sunrimii@gmail.com>
2023-02-10 08:30:32 +0800

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+# zsh
+apt update && apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+
+# zsh autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# zsh cd
+apt install zoxide
+
+# dotfiles
+cd ~
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply sunrimii
+
+source ~/.zshrc
