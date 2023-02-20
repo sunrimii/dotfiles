46e56207945dca4c7db93adc9d8a337ce7250f9b
JiaheEatingOut <jiahe@eating-out.co>
2023-02-20 23:19:20 +0800

    feat: use zplug to auto install plugin

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
-# zsh
-apt update && apt install zsh -y
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
-
-# zsh autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# zsh theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# zsh cd
-apt install zoxide
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # dotfiles
 chezmoi init sunrimii
 chezmoi update
+
 source ~/.zshrc
