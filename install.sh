944d99e27fe70741f57eedf45966352b3c0733a4
JiaheEatingOut <jiahe@eating-out.co>
2023-02-27 08:13:37 +0000

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ sudo apt install fish grc fzf zoxide fd-find exa bat
 # starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo "starship init fish | source" >>  ~/.config/fish/config.fish
+
+# git
+# cp dot_gitconfig ~/.gitconfig # vscode had already done for us.
+cp dot_gitignore_global ~/.gitignore_global
